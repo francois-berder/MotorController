56d70722dfa39264a3a495bae30fb91200c3f1e1
Francois Berder <fberder@outlook.fr>
2017-09-15 09:51:02 +0100

    Control motor from radio
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/main.c
+++ src/main.c
@@ -35,24 +35,46 @@
 
 #include <xc.h>
 #include "mcu.h"
+#include "motor.h"
 #include "periph_conf.h"
 #include "periph/gpio.h"
+#include "periph/timer0.h"
+#include "periph/timer1.h"
+#include "radio.h"
 #include "status.h"
 
 #define STATUS_LED          (GPIO_PIN(PORT_C, 0))
+#define RADIO_PIN           (GPIO_PIN(PORT_A, 4))
+
+#define LEFT_PWM_PIN        (GPIO_PIN(PORT_C, 3))
+#define RIGHT_PWM_PIN       (GPIO_PIN(PORT_C, 1))
+#define LEFT_PWM            (2)
+#define RIGHT_PWM           (4)
 
 int main()
 {
     mcu_init();
+    timer0_configure(TIMER0_PRESCALER_256);
 
     /* Change clock speed to 8MHz */
     OSCCON = (0b1110 << _OSCCON_IRCF_POSITION);
+
+    radio_init(RADIO_PIN);
+
+    motor_init(LEFT_PWM, RIGHT_PWM);
+    gpio_init_out(LEFT_PWM_PIN, 0);
+    gpio_init_out(RIGHT_PWM_PIN, 0);
 
     status_init(STATUS_LED);
     status_set_mode(STATUS_BLINK_FAST);
 
     while (1) {
+        motor_tick();
+        mcu_delay(1);
+        if (!radio_has_data())
+            continue;
 
+        motor_set_target(radio_get_data());
     }
 
     return 0;
